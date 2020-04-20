a74f5fc319c739b245956ad2082f459e20da074f
Pujan Thapa <42918326+pujan01@users.noreply.github.com>
2020-04-19 19:03:14 -0700

    Added insertion in Vendor table

--- flaskr/views/vendors.py
+++ flaskr/views/vendors.py
@@ -8,6 +8,16 @@ from flaskr.db import get_db, execute
 def viewVendorsHelper(conn):
     return execute(conn, "SELECT vendor_id, vendor_name, phone FROM Vendors")
 
+
+def insertVendorInDB(conn, vendor_name, phone):
+    print(vendor_name, phone)
+    if vendor_name in ("", None):
+        raise Exception
+    return execute(
+    conn,
+    "INSERT INTO Vendors (vendor_name, phone) VALUES (:vendor_name, :phone);", {'vendor_name': vendor_name, 'phone': phone}
+    )
+
 #  RenderFunctions
 def views(bp):
     @bp.route("/vendors")
@@ -15,3 +25,19 @@ def views(bp):
         with get_db() as conn:
             rows = viewVendorsHelper(conn)
         return render_template("table.html", name="Vendors", rows=rows)
+
+    @bp.route("/vendors/add", methods = ['POST', 'GET'])
+    def renderAddVendorForm():
+        attributes = {"VendorName" : "text", "Phone": "text" }
+        return render_template("form.html", name="Add Vendor: ", URI="/vendors/add/submit",  submit_message="Add Vendor", attributes=attributes)
+
+    @bp.route("/vendors/add/submit", methods = ['POST', 'GET'])
+    def AddVendor():
+        with get_db() as conn:
+            vendor_name = request.form.get("VendorName")
+            phone = request.form.get("Phone")
+            try:
+                insertVendorInDB(conn, vendor_name, phone)
+            except Exception:
+                return render_template("form_error.html", errors=["Your insertions did not went through check your inputs again."])
+        return viewVendors()
